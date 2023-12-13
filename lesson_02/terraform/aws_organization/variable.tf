b3bfe125bfacba03f93d7dee07e7da8b73a2aeb7
danlv3 <v.danlv3@vinfast.vn>
2023-12-14 06:25:25 +0700

    feat: homeWork 2

---
+++ lesson_02/terraform/aws_organization/variable.tf
@@ -0,0 +1,15 @@
+variable "trusted_arn_entity" {
+  default = "arn:aws:iam::734921941180:root"
+}
+
+variable "dev_account_name" {
+  default = "dev_danlv3_01"
+}
+
+variable "dev_email_address" {
+  default = "levandan.rrite@gmail.com"
+}
+
+variable "develop_ou" {
+  default = "develop-ou"
+}
